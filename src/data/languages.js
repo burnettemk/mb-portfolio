9dc5b61c0e7c1fe2969d8e0bb18f692047abc803
burnettemk <wafflesxyz@gmail.com>
2023-11-29 11:46:23 -0800

    Change Doc name

--- src/data/languages.js
+++ src/data/languages.js
@@ -1,7 +1,7 @@
 export const languages = [
   { id: 1, name: "Python", value: 16 },
-  { id: 2, name: "Javascript", value: 20.5 },
-  { id: 3, name: "CSS", value: 15.75 },
+  { id: 2, name: "Javascript", value: 21 },
+  { id: 3, name: "CSS", value: 16.75 },
   { id: 4, name: "Typescript", value: 5 },
   { id: 5, name: "Java", value: 9 },
   { id: 6, name: "C++" },
