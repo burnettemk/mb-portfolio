0da8c45e9f81bf721e6ec0e0da6e91f3813cf030
burnettemk <wafflesxyz@gmail.com>
2023-12-01 16:37:10 -0800

    Add tool

--- src/data/tools.js
+++ src/data/tools.js
@@ -1,8 +1,9 @@
 export const tools = [
-    {id: 23, name: "Github"},
-    {id: 96, name: "Git"},
-    {id: 76, name: "React"},
-    {id: 46, name: "VS Code"},
-    {id: 33, name: "Chakra UI"},
-    {id: 56, name: "Bootstrap"},
-]
+  { id: 23, name: "Github" },
+  { id: 96, name: "Git" },
+  { id: 76, name: "React" },
+  { id: 46, name: "VS Code" },
+  { id: 33, name: "Chakra UI" },
+  { id: 56, name: "Bootstrap" },
+  { id: 489, name: "ExpressJS" },
+];
