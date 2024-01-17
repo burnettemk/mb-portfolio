5a23c6ecdab8d9d5b22efcc00618b7e271c5dbcc
burnettemk <wafflesxyz@gmail.com>
2024-01-16 18:12:55 -0800

    Add title switch and click handler

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
+import { useState } from "react";
 import LanguageList from "./components/LanguageList";
 import LanguageToolList from "./components/LanguageToolList";
+import TitleSwitch from "./components/TitleSwitch.jsx";
 import ToolList from "./components/ToolList";
+import Experience from "./components/Experience.jsx";
 
 function App() {
+  const [selected, setSelected] = useState("");
+
   return (
     <>
+      <TitleSwitch />
       <LanguageList />
       <ToolList />
-      <LanguageToolList />
+      <LanguageToolList onSelect={setSelected} />
+      <div>{selected ? selected : "Nothing Selected"}</div>
     </>
   );
 }
